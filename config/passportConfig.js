d3f182cd3908927a44cbc593ffaae69b9c53216b
Ishwar Prajapati <prajapatiishwar79@gmail.com>
2025-08-19 14:11:38 +0530

    Update passportConfig.js

--- config/passportConfig.js
+++ config/passportConfig.js
@@ -1,18 +1,18 @@
 const LocalStrategy = require('passport-local').Strategy;
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const bcrypt = require('bcryptjs');
 const User = require('../models/User');
 
 module.exports = function(passport) {
+  // Local Strategy
   passport.use(
     new LocalStrategy({ usernameField: 'email' }, async (email, password, done) => {
       try {
-        // Match user
         const user = await User.findOne({ email: email });
         if (!user) {
           return done(null, false, { message: 'That email is not registered' });
         }
 
-        // Match password
         const isMatch = await bcrypt.compare(password, user.password);
         if (isMatch) {
           return done(null, user);
@@ -23,6 +23,49 @@ module.exports = function(passport) {
         return done(error);
       }
     })
+  );
+
+  // Google Strategy
+  passport.use(
+    new GoogleStrategy(
+      {
+        clientID: process.env.GOOGLE_CLIENT_ID,
+        clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+        callbackURL: '/auth/google/callback',
+      },
+      async (accessToken, refreshToken, profile, done) => {
+        try {
+          let user = await User.findOne({ googleId: profile.id });
+
+          if (user) {
+            return done(null, user);
+          } else {
+            // If the user already registered with email, link accounts
+            const existingUser = await User.findOne({ email: profile.emails[0].value });
+            if (existingUser) {
+              existingUser.googleId = profile.id;
+              existingUser.avatar = profile.photos[0].value;
+              await existingUser.save();
+              return done(null, existingUser);
+            }
+
+            // Otherwise create a new user
+            const newUser = new User({
+              name: profile.displayName,
+              email: profile.emails[0].value,
+              googleId: profile.id,
+              avatar: profile.photos[0].value,
+              password: 'N/A', // not used for Google users
+            });
+
+            await newUser.save();
+            return done(null, newUser);
+          }
+        } catch (err) {
+          return done(err, false);
+        }
+      }
+    )
   );
 
   passport.serializeUser((user, done) => {
